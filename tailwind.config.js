0087df607ab3cf856f9399a3b1568282118974c9
Billy Zimmerman <billyzman87@gmail.com>
2022-03-10 13:43:00 -0800

    added footer and nav bar, lots more work to come

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,64 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '14' : '14px',
+      }
+    },
+    container: {
+      padding: '1rem'
+    },
+    colors: {
+    transparent: 'transparent',
+
+    black: '#000',
+    white: '#fff',
+    blue: '#1c75bc',
+    'blue-dark': '#064f89',
+
+    green: {
+      100: '#f0fff4',
+      200: '#c6f6d5',
+      300: '#9ae6b4',
+      400: '#68d391',
+      500: '#48bb78',
+      600: '#38a169',
+      700: '#2f855a',
+      800: '#276749',
+      900: '#22543d',
+    },
+
+    gray: {
+      100: '#f7fafc',
+      200: '#edf2f7',
+      300: '#e2e8f0',
+      400: '#cbd5e0',
+      500: '#a0aec0',
+      600: '#718096',
+      700: '#4a5568',
+      800: '#2d3748',
+      900: '#1a202c',
+    },
   },
-  variants: {},
-  plugins: [],
+
+    fontFamily: {
+      sans: [
+        'Roboto',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+    },
+  }
 }
